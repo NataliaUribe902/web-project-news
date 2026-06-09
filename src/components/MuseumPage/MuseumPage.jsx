9a5dda2b815bc3b6a938f88e26b4e7b200d0c72a
Natalia <uribenatalia48@gmail.com>
2026-06-08 22:53:17 -0500

    corrected error

--- src/components/MuseumPage/MuseumPage.jsx
+++ src/components/MuseumPage/MuseumPage.jsx
@@ -4,6 +4,7 @@ import column1 from "../../images/columnPage1.jpg";
 import column2 from "../../images/columnPage2.jpg";
 import column3 from "../../images/ColumnPage3.jpg";
 import column4 from "../../images/ColumnPage4.jpg";
+import column5 from "../../images/ColumnPage5.jpg";
 import column6 from "../../images/ColumnPage6.jpg";
 
 function MuseumPage() {
