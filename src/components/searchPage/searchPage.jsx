040e5232a00646bfcd6da5bfc62dc34a21170d2e
Natalia <uribenatalia48@gmail.com>
2026-06-08 22:34:59 -0500

    added changes

--- src/components/searchPage/searchPage.jsx
+++ src/components/searchPage/searchPage.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 import CategoryPage from "../../components/CategoryPage/CategoryPage";
 
-import search1 from "../../images/searchPage1.jpg";
-import search2 from "../../images/searchPage2.jpg";
-import search3 from "../../images/searchPage3.jpg";
-import search4 from "../../images/searchPage4.jpg";
-import search5 from "../../images/searchPage5.jpg";
-import search6 from "../../images/searchPage6.jpg";
+import search1 from "../../images/searchpage1.jpg";
+import search2 from "../../images/searchpage2.jpg";
+import search3 from "../../images/searchpage3.jpg";
+import search4 from "../../images/searchpage4.jpg";
+import search5 from "../../images/searchpage5.jpg";
+import search6 from "../../images/searchpage6.jpg";
 
 function SearchPage() {
   const { query } = useParams();
